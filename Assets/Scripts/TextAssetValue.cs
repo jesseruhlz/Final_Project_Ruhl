03eb5184ed51a60b8b1cdee537d694bed02b51f3
jesseruhlz <jeruhl@chapman.edu>
2021-05-07 14:08:58 -0700

    New Dialogue UI
    Setup new UI elements to work with Ink, however I need to reimport all assets due to a corruption in the unity program.

---
+++ Assets/Scripts/TextAssetValue.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects / New Dialogue Value", fileName = "New Dialogue Asset")]
+
+public class TextAssetValue : ScriptableObject
+{
+
+    public TextAssetValue value;
+}
